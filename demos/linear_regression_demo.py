9caf50937dff69bcc7f88c2e46b8636e55a69812
cruxy <cruxyoung@hotmail.com>
2017-08-21 12:17:30 +1000

    needless code deleted in regression demo

--- demos/linear_regression_demo.py
+++ demos/linear_regression_demo.py
@@ -6,20 +6,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 
-fruits = pd.read_table('fruit_data.txt')
+# fruits = pd.read_table('fruit_data.txt')
 
-feature_names_fruits = ['height', 'width', 'mass', 'color_score']
-X_fruits = fruits[feature_names_fruits]
-y_fruits = fruits['fruit_label']
-target_names_fruits = ['apple', 'mandarin', 'orange', 'lemon']
+# feature_names_fruits = ['height', 'width', 'mass', 'color_score']
+# X_fruits = fruits[feature_names_fruits]
+# y_fruits = fruits['fruit_label']
+# target_names_fruits = ['apple', 'mandarin', 'orange', 'lemon']
 
-X_train, X_test, y_train, y_test = train_test_split(X_fruits, y_fruits, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(X_fruits, y_fruits, random_state=0)
 
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-# we must apply the scaling to the test set that we computed for the training set
-X_test_scaled = scaler.transform(X_test)
+# from sklearn.preprocessing import MinMaxScaler
+# scaler = MinMaxScaler()
+# X_train_scaled = scaler.fit_transform(X_train)
+# # we must apply the scaling to the test set that we computed for the training set
+# X_test_scaled = scaler.transform(X_test)
 
 X_R1, y_R1 = make_regression(n_samples = 100, n_features=1,
                             n_informative=1, bias = 150.0,
