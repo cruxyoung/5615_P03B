eac5e0cf67e2be15ccd852cc9d41a3d06e5d50a4
cruxy <cruxyoung@hotmail.com>
2017-08-29 15:16:27 +1000

    first behave test added

---
+++ features/browser_feature.feature
@@ -0,0 +1,9 @@
+Feature: The framework of machine learning
+
+Scenario: output the feature
+	Given load the data
+	When input print command
+	Then the feature of datasets is printed
+
+
+
---
+++ features/steps/browser_feature.py
@@ -0,0 +1,15 @@
+from behave import *
+from main import * 
+from pandas import DataFrame
+
+@given('load the data')
+def step_impl(context):
+	pass
+
+@when('input print command')
+def step_impl(context):
+	pass
+
+@then('the feature of datasets is printed')
+def step_impl(context):
+	pass
