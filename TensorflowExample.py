d492e27491acef714f7b91e18c07af8c647dc3b9
xili9459 <30790933+xili9459@users.noreply.github.com>
2017-09-01 00:26:46 +1000

    Add files via upload
    TensorflowExample

---
+++ TensorflowExample.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+
+# create data
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.1 + 0.3
+
+### create tensorflow structure start ###
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zeros([1]))
+
+y = Weights*x_data + biases
+
+loss = tf.reduce_mean(tf.square(y-y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+### create tensorflow structure end ###
+
+sess = tf.Session()
+# tf.initialize_all_variables() no long valid from
+# 2017-03-02 if using tensorflow >= 0.12
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+sess.run(init)
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(Weights), sess.run(biases))
